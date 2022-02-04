22c3fb9b1c091b6dfb4746d361c6f3cead8eb3be
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-04 10:22:13 +0000

    Added tool 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n = '0';
+	int s = ' ';
+	int v = ',';
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		putchar(v);
+		putchar(s);
+	}
+	putchar('\n');
+
+	return (0);
+}
