e7fc11b7f760b64f503bc36f2132c68adafe88b3
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-07 17:42:45 +0000

    added tool 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* main - Main func
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int n = '0';
+	int n2 = '0';
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (n2 = '0'; n2 <= '9'; n2++)
+		{
+			if (n < n2)
+			{
+				putchar(n);
+				putchar(n2);
+				
+				if ((n + n2) != ('8' + '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
