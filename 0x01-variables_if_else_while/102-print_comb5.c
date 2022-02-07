13a7dfbe379c9eb5827d5e5a795a3c0ec844d741
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-07 18:16:19 +0000

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+* main - Main func
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int n = '0';
+	int n2 = '0';
+	int n3 = '0';
+	int n4 = '0';
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (n2 = '0'; n2 <= '9'; n2++)
+		{
+			for (n3 = '0'; n3 <= '9'; n3++)
+			{
+				for (n4 = '0'; n4 <= '9'; n4++)
+				{
+					if ((n != n3 || n2 != n4) && (n * 10 + n2 < n3 * 10 + n4))
+					{
+						putchar(n);
+						putchar(n2);
+						putchar(' ');
+						putchar(n3);
+						putchar(n4);
+
+						if (n != '9' || n2 != '8' || n3 != '9' || n4 != '9')
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
