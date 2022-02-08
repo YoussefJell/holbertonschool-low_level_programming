c449597ed760c7a5ea97b576f9d6a7f4ceade55d
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-08 11:14:03 +0000

    Changed 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,13 @@
 */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+    int i;
+    char str[] = "_putchar";
 
+    for (i = 0; i < 8; i++)
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
 	return (0);
 }
