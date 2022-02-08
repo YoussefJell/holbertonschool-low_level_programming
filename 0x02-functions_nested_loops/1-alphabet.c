ab4d2cd33db78d11e23eda77a335bc5574da8a6d
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-08 11:19:19 +0000

    Fixed betty in 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,19 @@
 #include "main.h"
-/*
-* main - Entry point
+/**
+* print_alphabet - Prints the alphabet
 *
-* Return: 0 (Success)
+* Return: Always 0 (Success)
 */
+
 void print_alphabet(void)
 {
-    int i;
+	int i;
 
-    for (i = 97; i <= 122; i++)
-    {
-        _putchar(i);
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
 
-    }
-    _putchar('\n');
+	}
+	_putchar('\n');
 
 }
