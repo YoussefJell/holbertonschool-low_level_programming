cdbd52a5550ecf7f522facf9d2f7892ad344a3a3
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-08 11:20:28 +0000

    Fixed betty 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
 #include "main.h"
-/*
-* main - Entry point
+/**
+* print_alphabet_x10 - Prints alphabet 10 times
 *
-* Return: 0 (Success)
+* Return: Always 0 (Success)
 */
 void print_alphabet_x10(void)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    for (j = 0; j <= 9; j++)
-    {
-        for (i = 97; i <= 122; i++)
-        {
-            _putchar(i);
-        }
-        _putchar('\n');
-    }
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
 }
