47ff15a72fd1dcaf737189fe9ca97b37ac9d6d06
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-10 08:59:32 +0000

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers
+ *
+ * Return: void doesn't return
+ */
+void more_numbers(void)
+{
+	int i;
+	int s;
+
+	for (s = 0; s <= 9; s++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+			else
+			{
+				_putchar(i + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
