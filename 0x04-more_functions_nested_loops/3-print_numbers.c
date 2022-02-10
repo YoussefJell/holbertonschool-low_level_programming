d61869e7255746a0016eb12d3702eaaaa245c50c
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-10 08:48:48 +0000

    Added 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: void doesn't return
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
