ece34adb9e9f0f7d2a5c7a42580b6b55ba5f50a6
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-10 16:41:14 +0000

    Add triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_square - Prints a square
+ * @size: size of square (height and width)
+ * Return: void doesn't return
+ */
+void print_triangle(int size)
+{
+	int h;
+	int w;
+	int c = '#';
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (h = 0; h < size; h++)
+	{
+		for (w = 0; w < size; w++)
+		{
+			if (w >= size - h - 1)
+			{
+				_putchar(c);
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
