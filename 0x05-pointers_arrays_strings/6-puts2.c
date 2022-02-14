101b7ea58e6f8e63d946195891ec2b3bbdc6c7a7
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-14 11:34:32 +0000

    Added puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - outputs string
+ * @str: string
+ * Return: none, void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
