3705fae0789875e18756b3c26e70c9802ba05816
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-17 14:15:01 +0000

    Fix again

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,11 +10,14 @@ void reverse_array(int *a, int n)
 {
 	char tmp;
 	int i;
+	int j;
 
-	for (i = 0; i < n / 2; i++)
+	j = n - 1;
+
+	for (i = 0; i < (n / 2); i++)
 	{
 		tmp = a[i];
-		a[i] = a[n - 1];
-		a[n - 1 - 1] = tmp;
+		a[i] = a[j];
+		a[j - 1] = tmp;
 	}
 }
