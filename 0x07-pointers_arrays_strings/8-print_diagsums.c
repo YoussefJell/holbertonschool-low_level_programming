263da596817555e8823a353bb6fa308d82e9e262
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-21 16:08:08 +0000

    Added 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * @brief
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	int principal = 0, secondary = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				principal += a[(size * i) + j];
+
+			if ((i + j) == (size - 1))
+				secondary += a[(size * (i + 1)) - (i + 1)];
+		}
+	}
+	printf("%d, %d\n", principal, secondary);
+}
