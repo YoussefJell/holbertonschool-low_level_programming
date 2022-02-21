183992c1b5bec29f9ef9138c446768c6eaeefbc9
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-21 09:37:30 +0000

    Added 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,15 +1,38 @@
 #include "main.h"
+
+/**
+ * _strchr - locates character in string
+ * @b: string
+ * @c: characters
+ * Return: char
+ */
+
+char *_strchr(char *b, char c)
+{
+
+	for (; *b != '\0'; b++)
+		if (*b == c)
+			return (b);
+	return ((void *)0);
+}
+
 /**
  * _strspn - locates character in string
  * @accept: bytes
- * @s: number of bytes of initial segment
+ * @s: str
  * Return: unsigned int
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int len = 0;
 
-	for (; *s != '\0'; s++)
-		if (*s == c)
-			return (s);
-	return ((void *)0);
+	if ((s == ((void *)0)) || (accept == ((void *)0)))
+		return (len);
+
+	while (s && _strchr(accept, *s++))
+	{
+		len++;
+	}
+	return (len);
 }
