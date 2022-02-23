6807a1c8193fcc480bf0d27339b60d221b61b585
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-23 09:05:30 +0000

    Added 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts_recursion - puts with recursion
+ * @s: string
+ * Return: none, void
+ */
+void _puts_recursion(char *s)
+{
+
+	_putchar(*s++);
+
+	if (*s != '\0')
+	{
+		_puts_recursion(s);
+	}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+}
