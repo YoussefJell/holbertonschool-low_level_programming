b5e6a5b56dc00541854de1dc2128003fbfa94940
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-23 09:19:40 +0000

    fixed? 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,13 @@ void _puts_recursion(char *s)
 
 	_putchar(*s++);
 
-	if (*s != '\0')
-	{
-		_puts_recursion(s);
-	}
 	if (*s == '\0')
 	{
 		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }
