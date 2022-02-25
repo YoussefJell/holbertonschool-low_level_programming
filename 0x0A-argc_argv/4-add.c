d8c6d183396b7553ad5fb6492dcbe1aa60ffa70d
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-25 11:14:41 +0000

    Added 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,12 +20,15 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(*argv[i]))
-				sum += atoi(argv[i]);
-			else
+			if ((*argv[i] >= 65 && *argv[i] <= 90) ||
+				(*argv[i] >= 97 && *argv[i] <= 122))
 			{
 				printf("Error\n");
 				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
 			}
 		}
 		printf("%d\n", sum);
