4b1d0a6f73c71a77b8d1eb76356a2759e755860a
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-25 09:15:57 +0000

    Betty

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints the number of arguments passed into it
+ * @argc: argument count
+ * @argv: arguemnt array
  * Return: 0 (Success)
  */
 int main(int argc, char *argv[])
