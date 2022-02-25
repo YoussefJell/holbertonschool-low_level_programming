648013542ff4678cafa0f17fdb96656f473ed944
YoussefJell <youssef.jellouli@yahoo.com>
2022-02-25 09:20:21 +0000

    Comment fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the number of arguments passed into it
+ * main - prints arguments
  * @argc: argument count
  * @argv: arguemnt array
  * Return: 0 (Success)
