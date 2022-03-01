f17fc5fa336d173882c56f4f1f2227f292c76c25
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-01 10:41:41 +0000

    Added 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,17 +11,24 @@ char *_strdup(char *str)
 	int i, strlenstr;
 	char *p;
 
-	for (strlenstr = 0; str[strlenstr] != '\0'; strlenstr++)
-		;
-
-	p = (char *)malloc(sizeof(char) * strlenstr + 1);
-
-	if (p == NULL || str == NULL)
+	if (str == NULL)
 		return (NULL);
-
-	for (i = 0; i < strlenstr; i++)
+	else
 	{
-		p[i] = str[i];
+		for (strlenstr = 0; str[strlenstr] != '\0'; strlenstr++)
+			;
+
+		p = (char *)malloc(sizeof(char) * strlenstr + 1);
+
+		if (p == NULL)
+			return (NULL);
+
+		{
+			for (i = 0; i < strlenstr; i++)
+			{
+				p[i] = str[i];
+			}
+		}
 	}
 	return (p);
 }
