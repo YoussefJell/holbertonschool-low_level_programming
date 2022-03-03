888ea0a3ad1180bec59b02a25a59b973943acf16
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-03 11:00:16 +0000

    Added 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,14 +8,14 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
+	unsigned int *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	p = malloc(sizeof(unsigned int) * nmemb * size);
+	p = malloc(nmemb * size);
 	if (p == NULL)
 	{
 		return (NULL);
