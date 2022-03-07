9e14ed7c8382599919972ba77b4f3fb2df3ff9a5
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-07 08:58:05 +0000

    Added 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "dog.h"
-
+/**
+ * init_dog - initializes doggo structure
+ * @d: struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of dog
+ * Return: Void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	(*d).name = name;
