bfefc259324be9182ac9ca60b0685047f6ba70d0
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-07 08:37:13 +0000

    Betty

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #define FILENAME __FILE__
-
+/**
+ * main - prints filename before compilation
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	printf(FILENAME "\n");
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct my_dog - doggy struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of dog
+ *
+ * Description: This struct has the data of my_dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
