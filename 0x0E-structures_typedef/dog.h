2291062f49f55511e763f96278d082d24477e5d1
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-07 09:14:26 +0000

    last

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,10 @@
 #define HEADER_FILE
 
 /**
- * struct my_dog - doggy struct
+ * struct dog - doggy struct
  * @name: dog name
  * @age: dog age
  * @owner: owner of dog
- *
  * Description: This struct has the data of my_dog
  */
 struct dog
@@ -20,5 +19,7 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
