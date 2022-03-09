30ee7fdaaac52207c0ad3c489780aa04432a3658
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-09 14:11:51 +0000

    3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,10 +21,10 @@ int (*get_op_func(char *s))(int, int)
 	while (ops != NULL)
 	{
 		if (*s == ops[i].op[0])
+		{
 			return (ops[i].f);
+		}
 		i++;
-		break;
 	}
-	printf("Error\n");
 	return (NULL);
 }
