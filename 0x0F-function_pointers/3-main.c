b9e0e0f9eaefbe18c595fd7dcc3e7d13964f4368
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-09 14:18:10 +0000

    Added

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,6 +20,11 @@ int main(int argc, char *argv[])
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	i = get_op_func(argv[2]);
 	if (i == NULL)
 	{
