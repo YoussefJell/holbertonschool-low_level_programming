a2ee2aeb6b09f85005283c63a2049ac09fd384c9
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-17 08:40:56 +0000

    Added 1

---
+++ 0x12-singly_linked_lists/1_list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * list_len - returns number of elements in a linked list
+ * @h: head of list
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *ptr = NULL;
+	size_t i = 0;
+
+	if (h != NULL)
+	{
+		ptr = malloc(sizeof(h));
+		ptr = h;
+	}
+
+	while (ptr != NULL)
+	{
+		i++;
+		ptr = ptr->next;
+	}
+	return (i);
+}
