e2de1675048ac6cc0c22e06cc8d598803052411a
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-17 12:06:33 +0000

    Added first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void print_faster(void) __attribute__((constructor));
+/**
+ * print_faster - prints faster than main
+ * Return: none, void
+ */
+void print_faster(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
