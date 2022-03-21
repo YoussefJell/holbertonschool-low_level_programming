a94e92f5aaa0567027ea79d963389bc72975ac96
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-21 12:22:41 +0000

    6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * pop_listint - free the listint_t list
+ * @head: head of list
+ * Return: data of first head
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr = *head;
+	int data = (*head)->n;
+
+	*head = (*head)->next;
+	free(ptr);
+	return (data);
+}
