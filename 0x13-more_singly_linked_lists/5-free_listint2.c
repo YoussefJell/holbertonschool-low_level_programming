96bc0d9e99e3428ef07da300c441b0aa9a74dc38
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-21 11:56:47 +0000

    Added 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - free the listint_t list
+ * @head: head of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr = *head;
+
+	while (ptr->next != NULL)
+	{
+		*head = ptr->next;
+		free(ptr);
+		ptr = *head;
+	}
+	free(*head);
+	*head = NULL;
+}
