fbcf4737dd5ad0fbdc2e78132b7e4db35fe03a38
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-22 15:06:42 +0000

    add

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,6 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 				tmp = ptr->next;
 				ptr->next = ptr->next->next;
 				free(tmp);
+				return (1);
 			}
 			else
 			{
@@ -37,7 +38,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			tmp = *head;
 			*head = (*head)->next;
 			free(tmp);
+			return (1);
 		}
 	}
-	return (1);
+	return (-1);
 }
