156a76a4af763275a90824f14c941ac43b6161d8
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-22 16:14:31 +0000

    add

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,18 +7,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	if (head)
+	while (*head)
 	{
-		while (*head)
-		{
-			ptr = *head;
-			*head = (*head)->next;
-			free(ptr);
-		}
-	}
-	else
-	{
-		return;
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
 	}
 	free(*head);
 	head = 0;
