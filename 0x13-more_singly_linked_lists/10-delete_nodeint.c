a297d0c16246a3b1ace9b29c069d64df8130a1d3
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-22 15:04:12 +0000

    add

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *ptr = NULL, *tmp = NULL;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
