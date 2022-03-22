2ebe32720585aad2882b19394532ded6506b98fb
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-22 17:20:04 +0000

    fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				ptr = ptr->next;
 				i++;
 			}
-			if (idx < i)
+			if (idx > i)
 				return (NULL);
 		}
 		else
