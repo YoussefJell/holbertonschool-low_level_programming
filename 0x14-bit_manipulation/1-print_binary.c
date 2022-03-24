caa9e58476d8b8987a9f6c8d2b5946125912a629
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-24 14:09:32 +0000

    recursivity

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,23 @@
 #include "main.h"
 /**
+ * _fill - fill up on heap and empty 1's and 0's
+ * @n: input
+ * Return: void
+ */
+void _fill(unsigned long int n)
+{
+	if (n == 0)
+		return;
+
+	_fill(n >> 1);
+
+	if ((n & 1) == 1)
+		_putchar('1');
+
+	if ((n & 1) == 0)
+		_putchar('0');
+}
+/**
  * print_binary - prints binary
  * @n: unsigned long int input
  * Return: void
@@ -11,9 +29,5 @@ void print_binary(unsigned long int n)
 		_putchar('0');
 		return;
 	}
-	while (n)
-	{
-		_putchar((n & 1) + '0');
-		n >>= 1;
-	}
+	_fill(n);
 }
