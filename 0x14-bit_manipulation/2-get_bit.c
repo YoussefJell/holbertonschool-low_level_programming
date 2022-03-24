4fb3f0e8669772e799ab6259970ef29d368d5da9
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-24 13:04:19 +0000

    add

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_bit -  Get the bit object at index
+ * @n: number input
+ * @index: index to find bit
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bitStatus;
+
+	bitStatus = (n >> index) & 1;
+
+	return (bitStatus);
+}
