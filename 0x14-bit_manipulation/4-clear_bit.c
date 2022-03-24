3c369960d47eb89ada5cc335cf09612d8bbe4591
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-24 16:46:05 +0000

    add

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * set_bit - sets a value of bit to 0 in a given index
+ * @n: number
+ * @index: index
+ * Return: 1 or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = ~(1 << index);
+	int check;
+
+	if (index > 64)
+		return (-1);
+
+	check = *n &= mask;
+
+	if (check)
+		return (1);
+	else
+		return (-1);
+}
