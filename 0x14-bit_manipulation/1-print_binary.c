eee1cbd9cbd8e31b39309f885bba460461a8bc69
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-24 12:23:20 +0000

    add

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,33 +1,12 @@
 #include "main.h"
 /**
- * print_rev - outputs string in reverse
- * @s: string
- * Return: none, void
- */
-
-void print_rev(char *s)
-{
-	int i = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	while (i)
-	{
-		_putchar(s[--i]);
-	}
-}
-
-/**
- * print_binary - Print binary
+ * binary_to_uint - binary to unsigned int convertor
  *
- * @n: unsigned long int
- * Return: void
+ * @b: char array of 0's and 1's
+ * Return: 0 or unsigned int number
  */
 void print_binary(unsigned long int n)
 {
-	char arr[1024];
-	int i = 0;
-
 	if (n == 0)
 	{
 		_putchar('0');
@@ -44,17 +23,13 @@ void print_binary(unsigned long int n)
 	{
 		if ((n & 1) == 1)
 		{
-			arr[i] = '1';
-			i++;
+			_putchar('1');
 			n >>= 1;
 		}
 		else if ((n & 1) == 0)
 		{
-			arr[i] = '0';
-			i++;
+			_putchar('0');
 			n >>= 1;
 		}
-		arr[i] = '\0';
 	}
-	print_rev(arr);
 }
