badcffce75eb89b01d42f1eee7a0ce1f9f7d7ec1
YoussefJell <youssef.jellouli@yahoo.com>
2022-03-24 13:13:58 +0000

    add

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bitStatus;
 
-	if (n)
-		bitStatus = (n >> index) & 1;
-	else
+	bitStatus = (n >> index) & 1;
+
+	if (bitStatus != 0 && bitStatus != 1)
 		return (-1);
 
 	return (bitStatus);
