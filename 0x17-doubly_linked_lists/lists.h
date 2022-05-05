f70976c57353f7be5729a3257b9d1a98ac2d3d2b
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-05 18:48:20 +0000

    add

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -30,5 +30,6 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 void delete_first(dlistint_t **head);
+void delete_last(dlistint_t **head);
 
 #endif
