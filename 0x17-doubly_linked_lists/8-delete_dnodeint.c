5dc1546a700e1bc4d6b203cd6fe46372036ad871
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-05 10:16:40 +0000

    add

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes node at index
+ * @head: head node
+ * @index: index
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = NULL, *ptr;
+	unsigned int i = 0;
+
+	if (!head || !*head)
+		return (-1);
+
+	ptr = (*head);
+	if (index == 0)
+	{
+		delete_first(head);
+	}
+	tmp = *head;
+	while (tmp)
+	{
+		if (i == index - 1)
+		{
+			ptr = tmp;
+			tmp->next->prev = ptr;
+			tmp->next = ptr->next->next;
+			free(ptr);
+			return (1);
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	return (-1);
+}
+/**
+ * delete_first - deletes first node
+ * @head: head node
+ */
+void delete_first(dlistint_t **head)
+{
+	dlistint_t *ptr = *head, *tmp = NULL;
+
+	tmp = *head;
+	if (ptr->next)
+	{
+		ptr = ptr->next;
+		ptr->prev = tmp->prev;
+		*head = ptr;
+	}
+	else
+	{
+		*head = NULL;
+	}
+
+	free(tmp);
+}
