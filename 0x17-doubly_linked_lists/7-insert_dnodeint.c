c6b541cc38a1accff082204dba41901607fe47b5
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-05 17:59:26 +0000

    compatibility issue

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -44,3 +44,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	return (newNode);
 }
+/**
+ * get_dnodeint_at_index - Get the dnodeint at index object
+ *
+ * @head: head of list
+ * @index: index of node
+ * Return: Node at index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr = NULL;
+	unsigned int i = 0;
+
+	if (head)
+	{
+		ptr = head;
+		while (ptr)
+		{
+			if (i == index)
+				return (ptr);
+
+			ptr = ptr->next;
+			i++;
+		}
+	}
+	return (ptr);
+}
