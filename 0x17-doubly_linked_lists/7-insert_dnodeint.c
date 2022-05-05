43cbc38f6a5db1b16a6c8a36addea3041c065119
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-05 18:18:11 +0000

    add

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -47,23 +47,3 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	return (newNode);
 }
-/**
- * dlistint_len - lenght of Doubly linked list
- * @h: head of list
- * Return: Number of elements
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	const dlistint_t *ptr;
-	unsigned int count = 0;
-
-	if (h == NULL)
-		return (0);
-	ptr = h;
-	while (ptr != NULL)
-	{
-		count++;
-		ptr = ptr->next;
-	}
-	return (count);
-}
