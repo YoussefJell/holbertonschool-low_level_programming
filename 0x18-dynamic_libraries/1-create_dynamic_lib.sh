c106917f7f442a606ee17e9915ea632c9134b912
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-09 11:08:24 +0000

    add

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -fPIC -c *.c
-gcc -Wall -pedantic -Werror -Wextra -shared *.o -o liball.so
+gcc -Wall -pedantic -Werror -Wextra *.c -c -fPIC
+gcc -Wall -pedantic -Werror -Wextra *.o -shared -o liball.so
